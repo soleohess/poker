3f91837493f885dfd0e2b40051145e65b9a300eb
Soleo Hess <144173311+soleohess@users.noreply.github.com>
2025-11-18 15:30:49 -0800

    Drafting bot functions, Next pull GameInfoAPI

--- players/Soleo_bot.py
+++ players/Soleo_bot.py
@@ -1,4 +1,33 @@
+from typing import List, Dict, Any
+
+from bot_api import PokerBotAPI, PlayerAction, GameInfoAPI
+from engine.cards import Card, Rank
+from engine.poker_game import GameState
+
+
 class SoleoBot(PokerBotAPI):
-    def get_action(self, game_state, hole_cards, legal_actions, min_bet, max_bet):
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.hands_played = 0
+        self.hands_won = 0
+        #can define good starting hands here
+
+    def get_action(self, game_state, hole_cards, legal_actions, min_bet, max_bet) -> tuple:
         #poker strategy goes here
+        
         return PlayerAction.CALL, 0
+
+
+
+    # Calculate pot odds
+    pot_odds = GameInfoAPI.get_pot_odds(game_state.pot, amount_to_call)
+
+    # Get position information
+    position_info = GameInfoAPI.get_position_info(game_state, self.name)
+
+    # Check if heads-up
+    is_heads_up = GameInfoAPI.is_heads_up(game_state)
+
+    # Get opponent list
+    opponents = GameInfoAPI.get_active_opponents(game_state, self.name)
